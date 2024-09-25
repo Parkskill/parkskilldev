5136c7adebc1beba0e5dda0255788dad374a373f
Anvesh Vankalapati <anvesh.vankalapati@gsa.gov>
2024-09-25 16:16:06 -0400

    update room scenes page

--- ParkSkills_Core/ClientApp/src/app/components/room-scenes/room-scenes.component.ts
+++ ParkSkills_Core/ClientApp/src/app/components/room-scenes/room-scenes.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApicallService } from 'src/app/shared/apicall.service';
 
 @Component({
   selector: 'room-scenes',
   templateUrl: './room-scenes.component.html',
-  styleUrls: ['./room-scenes.component.scss']
+  styleUrls: ['./room-scenes.component.scss'],
 })
 export class RoomScenesComponent implements OnInit {
+  loading = true;
+  public roomScenesCollection: any = {};
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    public apicallService: ApicallService
+  ) {
+    this.getAllTiles();
   }
 
+  ngOnInit(): void {}
+
+  getAllTiles() {
+    this.loading = true;
+    this.apicallService.getAllTiles().subscribe({
+      next: (httpResponse) => {
+        console.log(httpResponse);
+        this.roomScenesCollection = httpResponse;
+      },
+
+      error: (error) => {
+        console.log('Error', error);
+      },
+      complete: () => {
+        this.loading = false;
+        console.log('Completed');
+      },
+    });
+  }
 }
