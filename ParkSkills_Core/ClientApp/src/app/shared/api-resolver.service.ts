393f8f58166084364033c5316c8a9c19b8b2b3bf
Anvesh Vankalapati <anvesh.vankalapati@gsa.gov>
2024-09-25 21:11:03 -0400

    upgrade to 16

--- ParkSkills_Core/ClientApp/src/app/shared/api-resolver.service.ts
+++ ParkSkills_Core/ClientApp/src/app/shared/api-resolver.service.ts
@@ -1,14 +1,14 @@
 // File: shared/api-resolver.service.ts
 
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ApicallService } from './apicall.service';  // Adjust path if necessary
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiResolver implements Resolve<any> {
+export class ApiResolver  {
 
   constructor(private apiService: ApicallService) { }
 
